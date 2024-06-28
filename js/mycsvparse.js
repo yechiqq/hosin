186885172b9a2df2551fef09b994852c54b800d5
yechiqq <yechi_qq@hotmail.com>
2024-06-28 08:58:03 +0800

    init

--- js/mycsvparse.js
+++ js/mycsvparse.js
@@ -7,7 +7,18 @@ $(document).ready(function() {
      });
 });
 
+const IID = 0;
+const INAME = 1;
+const ISUBTITLE = 2;
+const ICATEGORY = 3;
+const ISIZE = 4;
+const ISIZE2 =5;
+const IREMAKRS = 6;
+const IIMAGE = 7;
+const IDOCUMENT = 8;
+
 function processData(allText) {
     var data = $.csv.toObjects(allText);
+    $('#productCat').text(data[0].category);
     alert("csv lines: "+data.length);
 }
