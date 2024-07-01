423528564d14c08de2fe66dddeca20166e77f426
yechiqq <yechi_qq@hotmail.com>
2024-07-01 12:29:48 +0800

    Update myproducts.js

--- js/myproducts.js
+++ js/myproducts.js
@@ -12,14 +12,18 @@ $(document).ready(function() {
         if (searchword) {
             searchword = searchword.toLowerCase();
             $("#productsContainer").html("Search Result:<br/>");
+            $('#productsContainer').append('<div class="row">');
+            html = "";
             objectArray.forEach(function(item) {
                 // do something with `item`
                 if (item.productName.toLowerCase().indexOf(searchword) >= 0) {
-                    html = item.productName;
+                    html += '<div class="col-sm-6 col-md-4 isotope-item">';
+                    html += '<a class="" href="product.html?productId='+item.productId+'"><div class="image-box"><div class="overlay-container"><img class="img-fluid" src="productimage/'+item.image+'" alt="No Image"></div></div><div class="featurename">'+item.productName+'</div></a>';
+                    html += '</div>';
                     $('#productsContainer').append(html);
-                    return true;
                 }
             });
+            $('#productsContainer').append('</div>');
         }
     });
 });
