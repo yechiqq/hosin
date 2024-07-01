5e6c82771eb064c9a05aed37b4e3db740673adcf
yechiqq <yechi_qq@hotmail.com>
2024-07-01 12:31:54 +0800

    Update myproducts.js

--- js/myproducts.js
+++ js/myproducts.js
@@ -13,11 +13,11 @@ $(document).ready(function() {
             searchword = searchword.toLowerCase();
             $("#productsContainer").html("Search Result:<br/>");
             $('#productsContainer').append('<div class="row">');
-            html = "";
+            
             objectArray.forEach(function(item) {
                 // do something with `item`
                 if (item.productName.toLowerCase().indexOf(searchword) >= 0) {
-                    html += '<div class="col-sm-6 col-md-4 isotope-item">';
+                    html = '<div class="col-sm-6 col-md-4 isotope-item">';
                     html += '<a class="" href="product.html?productId='+item.productId+'"><div class="image-box"><div class="overlay-container"><img class="img-fluid" src="productimage/'+item.image+'" alt="No Image"></div></div><div class="featurename">'+item.productName+'</div></a>';
                     html += '</div>';
                     $('#productsContainer').append(html);
