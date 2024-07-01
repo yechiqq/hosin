a4e60eaa6914342c161faf6485ba6c08ac3bd708
yechiqq <yechi_qq@hotmail.com>
2024-07-01 13:52:58 +0800

    init

--- js/myproducts.js
+++ js/myproducts.js
@@ -12,20 +12,19 @@ $(document).ready(function() {
         if (searchword) {
             searchword = searchword.toLowerCase();
             $("#productsContainer").html("Search Result:<br/>");
-            $('#productsContainer').append('<div class="row">');
+            html = '<div class="row">';
 
             for (let i = 0; i < objectArray.length; ++i) {
                 // do something with `substr[i]`
                 item = objectArray[i];
                 if (item.productName.toLowerCase().indexOf(searchword) >= 0) {
-                    html = '<div class="col-sm-6 col-md-4 isotope-item">';
+                    html += '<div class="col-sm-6 col-md-4 isotope-item">';
                     html += '<a class="" href="product.html?productId='+item.productId+'"><div class="image-box"><div class="overlay-container"><img class="img-fluid" src="productimage/'+item.image+'" alt="No Image"></div></div><div class="featurename">'+item.productName+'</div></a>';
                     html += '</div>';
-                    $('#productsContainer').append(html);
                 }
             }
-            
-            $('#productsContainer').append('</div>');
+            html += '</div>';
+            $('#productsContainer').append(html);
         }
     });
 });
